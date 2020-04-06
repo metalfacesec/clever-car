c5401b5513894e23b5ea9c6f863d2a0d8b7e6032
Metal Face <metalfacesec@gmail.com>
2020-04-06 00:16:03 -0400

    Music player

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const app = require('express')();
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const GPS = require('gps');
 const path = require('path');
 const fs = require('fs');
 
@@ -19,24 +16,15 @@ app.get('/emulate', function (req, res) {
     const { exec } = require('child_process');
 
     let child = exec('higan --fullscreen "/home/pi/repo/clever-car-server/roms/nes/' + req.query.game + '"', {}, function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-      
         if (error !== null) {
             console.log('exec error: ' + error);
         }
-        res.send({});
     });
 
-    // child.on('exit', function (code) {
-    //   console.log('Child process exited with exit code '+code);
-    //   res.send({});
-    // });
+    res.send({});
 });
 
 app.get('/games', async function (req, res) {
-    console.log(req);
-
     try {
         let game_list = await getNesGames();
         res.json({status: 200, data: game_list});
@@ -45,6 +33,53 @@ app.get('/games', async function (req, res) {
         res.json({status: 400, data: []});
     }
 });
+
+app.get('/music', async function (req, res) {
+    try {
+        let music_list = await getMusicDirs();
+        res.json({status: 200, data: music_list});
+    } catch (err) {
+        console.log(err);
+        res.json({status: 400, data: []});
+    }
+});
+
+app.get('/tracks', async function (req, res) {
+    try {
+        let folder = req.query.folder;
+
+        let track_list = await getTracks(folder);
+        res.json({status: 200, data: track_list});
+    } catch (err) {
+        console.log(err);
+        res.json({status: 400, data: []});
+    }
+});
+
+function getMusicDirs() {
+    return new Promise(function (resolve, reject) {
+        const directoryPath = path.join(__dirname, 'music');
+
+        fs.readdir(directoryPath, function (err, files) {
+            if (err) { return reject(err); } 
+        
+            resolve(files);
+        }); 
+    });
+}
+
+function getTracks(folder) {
+    // TODO: File type filter
+    return new Promise(function (resolve, reject) {
+        const directoryPath = path.join(__dirname, 'music/' + folder);
+
+        fs.readdir(directoryPath, function (err, files) {
+            if (err) { return reject(err); } 
+        
+            resolve(files);
+        }); 
+    });
+}
 
 function getNesGames() {
     return new Promise(function (resolve, reject) {
@@ -57,24 +92,3 @@ function getNesGames() {
         }); 
     });
 }
-
-const port = new SerialPort('/dev/ttyAMA0', {baudRate: 9600});
-const parser = port.pipe(new Readline({delimiter: '\r\n'}));
-var gps = new GPS;
-
-io.on('connect', onConnect);
-
-function onConnect(socket) {
-    console.log("Connection!");
-}
-
-gps.on('data', data => {
-    if (data.type == "GGA" && data.quality != null) {
-        console.log('sending data now on socket io');
-        io.sockets.emit('coord_update', JSON.stringify({lat: data.lat, lon: data.lon}));
-    }
-});
-
-parser.on('data', data => {
-    gps.update(data);
-});
