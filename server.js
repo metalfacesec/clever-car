2fcfa0dbd69450357842589f7224944f79f8a3da
Metal Face <metalfacesec@gmail.com>
2020-04-06 00:38:54 -0400

    Get videos

--- server.js
+++ server.js
@@ -56,9 +56,21 @@ app.get('/tracks', async function (req, res) {
     }
 });
 
+app.get('/videos', async function (req, res) {
+    console.log('!!!');
+    try {
+        let track_list = await getVideos();
+        console.log(track_list);
+        res.json({status: 200, data: track_list});
+    } catch (err) {
+        console.log(err);
+        res.json({status: 400, data: []});
+    }
+});
+
 function getMusicDirs() {
     return new Promise(function (resolve, reject) {
-        const directoryPath = path.join(__dirname, 'music');
+        let directoryPath = path.join(__dirname, '../clever-car-client/public/music');
 
         fs.readdir(directoryPath, function (err, files) {
             if (err) { return reject(err); } 
@@ -71,7 +83,7 @@ function getMusicDirs() {
 function getTracks(folder) {
     // TODO: File type filter
     return new Promise(function (resolve, reject) {
-        const directoryPath = path.join(__dirname, 'music/' + folder);
+        let directoryPath = path.join(__dirname, '../clever-car-client/public/music/' + folder);
 
         fs.readdir(directoryPath, function (err, files) {
             if (err) { return reject(err); } 
@@ -80,6 +92,25 @@ function getTracks(folder) {
         }); 
     });
 }
+
+function getVideos() {
+    return new Promise(function (resolve, reject) {
+        let directoryPath = path.join(__dirname, '../clever-car-client/public/video/');
+        console.log(directoryPath);
+
+        fs.readdir(directoryPath, function (err, files) {
+            if (err) { return reject(err); }
+
+            resolve(files.filter((file) => {
+                if (file.endsWith('.avi')) {
+                    return true;
+                }
+                return false;
+            }));
+        }); 
+    });
+}
+
 
 function getNesGames() {
     return new Promise(function (resolve, reject) {
